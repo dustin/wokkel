e4afa35b1c3ea678f6486bd3937856fa9ac1d7e7
Fabio Forno <fabio.forno@gmail.com>
2008-12-24 14:12:52 -0800

    A whitespace-based keepalive.

---
+++ wokkel/keepalive.py
@@ -0,0 +1,21 @@
+from twisted.python import log
+from twisted.internet import task
+from subprotocols import XMPPHandler
+
+# From https://mailman.ik.nu/pipermail/twisted-jabber/2008-October/000171.html
+class KeepAlive(XMPPHandler):
+
+    interval = 300
+    lc = None
+
+    def connectionInitialized(self):
+        self.lc = task.LoopingCall(self.ping)
+        self.lc.start(self.interval)
+
+    def connectionLost(self, *args):
+        if self.lc:
+            self.lc.stop()
+
+    def ping(self):
+        log.msg("Stayin' alive")
+        self.send(" ")
